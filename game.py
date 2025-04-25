c88bed834d7927cc962003814ee76c71e02c42f9
akalyasri <akalya.s.sri@gmail.com>
2025-04-25 00:11:25 -0700

    getting size of board from user

--- game.py
+++ game.py
@@ -22,3 +22,18 @@ user = None
 board = None  # in the begining, none
 ai_turn = False
 
+# asking user for size of board they want to play
+def get_board_size():
+    while True:
+        try:
+            size_input = int(input("Enter the size of the Tic-Tac-Toe board (nxn): "))
+            if size_input < 3:
+                print("The board size should be at least 3x3.")
+            else:
+                return size_input
+        except ValueError:
+            print("Please enter a valid integer.")
+
+# creating board based on user input
+board_size = get_board_size()  # board size from the user
+board = ttt.initial_state(board_size)  # pass the size to your gameMaster's initial state
